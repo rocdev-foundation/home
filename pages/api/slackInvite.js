f9883a01251aa683ceb68083df5198cd1c4d6d7b
Josh Kramer <josh.kramer@measuresforjustice.org>
2020-10-01 21:04:00 -0400

    Forward request to Slack

--- pages/api/slackInvite.js
+++ pages/api/slackInvite.js
@@ -1,13 +1,29 @@
-// # Slack API config
-// SLACK_INVITE_API_BASE = os.getenv(
-//     'ROCDEV_SLACK_INVITE_API_BASE',
-//     default=)
-// SLACK_INVITE_API_TOKEN = os.getenv(
-//     'ROCDEV_SLACK_INVITE_API_TOKEN', default='not_a_real_token')
-
 export default function slackInvite(req, res) {
-  console.log(req.body);
-  // return fetch('https://slack.com/api/users.admin.invite', {
+  const email = req.body.email;
 
-  // })
+  const body = {
+    channel_ids: process.env.ROCDEV_SLACK_CHANNEL_IDS,
+    email,
+    team_id: process.env.ROCDEV_SLACK_TEAM_ID,
+    custom_message:
+      "Come join the biggest development community in Rochester, NY",
+    resend: true,
+  };
+
+  return fetch("https://slack.com/api/admin.users.invite", {
+    method: "post",
+    headers: {
+      Authorization: `Bearer ${process.env.ROCDEV_SLACK_INVITE_API_TOKEN}`,
+      "content-type": "application/json",
+    },
+    body: JSON.stringify(body),
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((json) => {
+      // Need to get a token before testing
+      console.log(json);
+      res.end(200);
+    });
 }
