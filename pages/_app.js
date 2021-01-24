694667a81385471587be3ca928a05ce604007ca4
George Adams IV (geowa4) <oss@geowa4.software>
2021-01-24 03:27:06 +0000

    filled in the slack and meetup tiles

--- pages/_app.js
+++ pages/_app.js
@@ -14,19 +14,21 @@ function MyApp({ Component, pageProps }) {
           rel="stylesheet"
         />
       </Head>
-      <div className={"font-sans"}>
+      <div className="font-sans flex flex-col min-h-screen">
         <MDXProvider components={htmlReplacements}>
-          <main className="w-5/6 lg:w-3/5 mx-auto">
+          <main className="flex-1 w-5/6 md:w-3/5 xl:w-2/5 mx-auto">
             <Component {...pageProps} />
           </main>
         </MDXProvider>
         <footer className="mt-8 border-t border-gray-300 p-8 text-center">
-          This work is licensed under a{" "}
-          <Anchor href="https://creativecommons.org/licenses/by-nc/4.0/legalcode">
-            Creative Commons Attribution-NonCommercial 4.0 International Public
-            License
-          </Anchor>
-          .
+          <div className="w-2/5 lg:w-1/5 mx-auto">
+            This work is licensed under a{" "}
+            <Anchor href="https://creativecommons.org/licenses/by-nc/4.0/legalcode">
+              Creative Commons Attribution-NonCommercial 4.0 International
+              Public License
+            </Anchor>
+            .
+          </div>
         </footer>
       </div>
     </>
--- pages/index.js
+++ pages/index.js
@@ -4,30 +4,74 @@ import { Anchor, Heading, Text } from "../components";
 
 export default function Home() {
   return (
-    <>
-      <Heading.H1 className={"text-center"}>
-        <Image src="/rocdev.svg" alt="RocDev logo" width={500} height={250} />
-      </Heading.H1>
-      <Text>
-        As the Greater Rochester Area's largest community of tech professionals,
-        we aim to share our experiences and knowledge with one another in an
-        effort to support and grow as a whole. In disciplines related to
-        Software & Development such as Software Engineering, Product Management,
-        and Quality Assurance; our hope is to promote the quality and profile of
-        the Rochester Tech scene. Our members are made up of Contributors,
-        Managers, Entrepreneurs, Students, and Teachers; all with a passion for
-        a more skilled and equitable software development ecosystem.
-      </Text>
+    <div className="text-justify">
+      <section>
+        <Heading.H1 className={"text-center"}>
+          <Image src="/rocdev.svg" alt="RocDev logo" width={500} height={250} />
+        </Heading.H1>
+        <Text>
+          As the Greater Rochester Area's largest community of tech
+          professionals, we aim to share our experiences and knowledge with one
+          another in an effort to support and grow as a whole. In disciplines
+          related to Software & Development such as Software Engineering,
+          Product Management, and Quality Assurance; our hope is to promote the
+          quality and profile of the Rochester Tech scene. Our members are made
+          up of Contributors, Managers, Entrepreneurs, Students, and Teachers;
+          all with a passion for a more skilled and equitable software
+          development ecosystem.
+        </Text>
+      </section>
 
-      <Text>
-        Read the{" "}
-        <Link href="/code-of-conduct">
-          <Anchor>Code of Conduct</Anchor>
-        </Link>
-      </Text>
+      <hr className="mt-8" />
 
-      <Heading.H3>Slack</Heading.H3>
-      <Heading.H3>Meetup</Heading.H3>
-    </>
+      <section>
+        <Heading.H3>Code of Conduct</Heading.H3>
+        <Text>
+          The health and wealthfare of our members are paramount. Therefore, we
+          ask that you come in with a positive and inquisitive attitude and
+          treat people with respect.
+        </Text>
+        <Text>
+          Please read the{" "}
+          <Link href="/code-of-conduct">
+            <Anchor>Code of Conduct</Anchor>
+          </Link>
+          .
+        </Text>
+      </section>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-x-8">
+        <section>
+          <Heading.H3>Slack</Heading.H3>
+          <Text>
+            Everyday we chat on Slack helping each other with a baffling stack
+            trace, talking about the latest tech news, and sharing career
+            advice. We have channels for popular languages like JavaScript and
+            Python as well as popular topics like cryptocurrencies and working
+            remotely.
+          </Text>
+          <Text>
+            {/* generated from https://rocdev.slack.com/admin/shared_invites */}
+            <Anchor href="https://join.slack.com/t/rocdev/shared_invite/zt-hcnnt9ya-qlcLd8C0cF9Z0wksTS9G3g">
+              Join our Slack workspace.
+            </Anchor>
+          </Text>
+        </section>
+        <section>
+          <Heading.H3>Meetup</Heading.H3>
+          <Text>
+            We gather at least once per month to learn something from fellow
+            members, hack on projects, or just hang out. Use the links below to
+            see what else is happening and get notified when another event is
+            scheduled.
+          </Text>
+          <Text>
+            <Anchor href="https://www.meetup.com/RocDev">
+              Join our Meetup group.
+            </Anchor>
+          </Text>
+        </section>
+      </div>
+    </div>
   );
 }
